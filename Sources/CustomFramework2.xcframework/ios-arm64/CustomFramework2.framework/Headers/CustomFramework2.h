41d472d7c6d5dc31f0db16fc4e8e97d6dbe3bb8c
Filip Cecelja <filip.cecelja@porsche-ep.com>
2023-12-12 15:48:11 +0100

    Add CustomFramework2

---
+++ Sources/CustomFramework2.xcframework/ios-arm64/CustomFramework2.framework/Headers/CustomFramework2.h
@@ -0,0 +1,18 @@
+//
+//  CustomFramework2.h
+//  CustomFramework2
+//
+//  Created by Filip Cecelja on 12-12-2023.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CustomFramework2.
+FOUNDATION_EXPORT double CustomFramework2VersionNumber;
+
+//! Project version string for CustomFramework2.
+FOUNDATION_EXPORT const unsigned char CustomFramework2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CustomFramework2/PublicHeader.h>
+
+
