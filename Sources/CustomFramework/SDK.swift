a4363e47ad34195016a062c4217db23d2747c8f5
Filip Cecelja <filip.cecelja@porsche-ep.com>
2023-12-12 14:17:52 +0100

    Initial commit

---
+++ Sources/CustomFramework/SDK.swift
@@ -0,0 +1,14 @@
+//
+//  SDK.swift
+//  CustomFrameworkProject
+//
+//  Created by Filip Cecelja on 12-12-2023.
+//
+
+import Foundation
+
+public struct SDK {
+    public static func doSomeWork() {
+        print("Doing some work...")
+    }
+}
